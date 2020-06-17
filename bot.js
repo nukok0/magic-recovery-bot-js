653acc37e223d8f8476acc2818c628ae31ab2be6
ぬこ <54257247+nukok0@users.noreply.github.com>
2020-06-17 20:45:23 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);
